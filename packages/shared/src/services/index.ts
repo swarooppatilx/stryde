b043047946583b37c0e420db2295dfd9bb2f7875
Swaroop Patil <swarooppatil850@gmail.com>
2026-09-05 17:34:46 +0530

    feat: add challenge service and barrel export

--- packages/shared/src/services/index.ts
+++ packages/shared/src/services/index.ts
@@ -1,4 +1,8 @@
+export * as achievement from './achievement';
 export * as activity from './activity';
+export * as challenge from './challenge';
 export * as client from './client';
 export * as profile from './profile';
+export * as season from './season';
+export * as sync from './sync';
 export * as territory from './territory';
